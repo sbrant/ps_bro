dda095113c21fb80fd38234295a3b524f4289275
Steve Brant <steve@brant.nu>
2018-02-04 04:29:38 -0700

    write out json

--- bro_conv.ps1
+++ bro_conv.ps1
@@ -11,5 +11,5 @@ foreach($line in Get-Content ~/temp/dns.log) {
         Write-Output $line | Out-File -Append -FilePath "~/temp/dns.clean.csv"
     }
 }
-Import-Csv ~/temp/dns.clean.csv -Header $split_line -Delimiter `t | ConvertTo-Json
+Import-Csv ~/temp/dns.clean.csv -Header $split_line -Delimiter `t | ConvertTo-Json -Compress | Out-File -FilePath "~/temp/dns.json"
 
